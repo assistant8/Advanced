2fb428ac881a39e21f052551a2b227f598589e3f
JoSooha <dlrhdcjs@naver.com>
2023-03-08 06:45:18 +0900

    1주차 수업

---
+++ react-app/src/Hello.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+const element = <h1>hello world</h1>
+function Hello() {
+    return (
+        <div>
+            {element}
+        </div>
+    );
+}
+
+export default Hello
